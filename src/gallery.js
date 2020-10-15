6bae7e93aae8190a3a898a58dacf1245ccd15b43
Max <mail@max52.33mail.com>
2020-10-15 15:06:17 -0400

    init repo

---
+++ src/gallery.js
@@ -0,0 +1,48 @@
+function addDOM(result) {
+  let data = JSON.parse(result);
+  let dom = document.querySelector("#dom");
+  let fragment = document.createDocumentFragment();
+
+  data.images.forEach((e) => {
+    let wrapper = document.createElement("div");
+    wrapper.onclick = function () {
+      location.href = "projects.html";
+    };
+    wrapper.className =
+      "column is-one-quarter-desktop hero is-one-third-tablet is-half-mobile";
+    let img = document.createElement("img");
+    img.className = "galleryimg";
+    let more = document.createElement("div");
+    more.className = "viewmore subtitle has-text-centered";
+    let p = document.createElement("p");
+    p.className = "viewmoretext";
+    p.innerText = "+";
+
+    fragment.appendChild(wrapper);
+    img.src = e;
+    wrapper.appendChild(img);
+    wrapper.appendChild(more);
+    more.appendChild(p);
+  });
+
+  dom.appendChild(fragment);
+}
+
+function readJSON(path) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", path, true);
+  xhr.responseType = "blob";
+  xhr.onload = function (e) {
+    if (this.status == 200) {
+      var file = new File([this.response], "temp");
+      var fileReader = new FileReader();
+      fileReader.addEventListener("load", function () {
+        addDOM(fileReader.result);
+      });
+      fileReader.readAsText(file);
+    }
+  };
+  xhr.send();
+}
+
+readJSON("./src/gallery.json");
